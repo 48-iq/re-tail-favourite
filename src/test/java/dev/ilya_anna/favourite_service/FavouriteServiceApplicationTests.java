285088ca4e73d24e71b800cb466a3ff58076a48b
48-iq <ivn25ov@mail.ru>
2025-03-22 14:27:43 +0400

    init

---
+++ src/test/java/dev/ilya_anna/favourite_service/FavouriteServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.ilya_anna.favourite_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class FavouriteServiceApplicationTests {
+    @Test
+    void helloWorld() {
+        System.out.println("Hello world 4");
+    }
+}
